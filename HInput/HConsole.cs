338de66c21c461427491f533fb01440772d7a6bd
Ignas <ignasmaslinskas@gmail.com>
2017-09-09 15:10:43 +0300

    Added initial implementation of console/input interface.

---
+++ HInput/HConsole.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreClient.HInput
+{
+    public class HConsole
+    {
+        private TextReader inputReader;
+        private TextWriter outputWriter;
+        private HConsoleProcessor hProcessor = new HConsoleProcessor();
+        public bool Running { get; set; } = false;
+
+        public HConsole()
+        {
+            inputReader = Console.In;
+            outputWriter = Console.Out;
+        }
+
+        public async Task StartReadingInputTask()
+        {
+            Running = true;
+            
+            while (Running)
+            {
+                var input = await inputReader.ReadLineAsync();
+                var response = await hProcessor.ProcessMessageTask(input);
+                await WriteToConsoleTask(response);
+            }
+        }
+
+
+        public async Task WriteToConsoleTask(string data)
+        {
+            if (Running)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                await outputWriter.WriteLineAsync("[USER]" + data);
+                await outputWriter.FlushAsync();
+            }
+        }
+    }
+}
---
+++ HInput/HConsoleProcessor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ChatProtos.Networking;
+
+namespace CoreClient.HInput
+{
+    class HConsoleProcessor
+    {
+        private IDictionary<string, ChatProtos.Networking.RequestType> _commands;
+
+        public HConsoleProcessor()
+        {
+            _commands = new Dictionary<string, ChatProtos.Networking.RequestType>()
+            {
+                { "/login", RequestType.Login },
+                { "/logout", RequestType.Logout },
+                { "/join", RequestType.JoinChannel },
+                { "/leave", RequestType.LeaveChannel },
+                { "/chat", RequestType.ChatMessage }
+            };
+        }
+
+        public async Task<String> ProcessMessageTask(string message)
+        {
+            var command = message.Split(' ')[0];
+            string response = "";
+            try
+            {
+                if (_commands[command] != null)
+                {
+                    response = "exists";
+                }
+            }
+            catch (KeyNotFoundException e)
+            {
+                response = "doesnt";
+            }
+            return response;
+        }
+
+        public async Task<RequestMessage> MakeRequestMessageTask(string message)
+        {
+            
+        }
+    }
+}
